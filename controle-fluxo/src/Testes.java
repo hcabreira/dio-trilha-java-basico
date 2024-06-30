c9b75c28ac54e7f0e8af5e35506dd02893fd765c
hcabreira <henrique@cabreira.com.br>
2024-06-30 16:15:54 -0300

    commit testes final de excecoes

--- controle-fluxo/src/Testes.java
+++ controle-fluxo/src/Testes.java
@@ -1,6 +1,33 @@
+import java.util.*; 
 public class Testes {
     public static void main(String[] args) {
-        String[] nomes = {"Camila", "Venilton", "Leonardo", "Renan", "Rafael"};
-System.out.print(nomes.length);
+        Scanner leitor = new Scanner(System.in);
+      int N = leitor.nextInt();
+      String jogador1, jogador2;
+// TODO: complete os espaços em branco com sua solução para o problema
+      for (int i = 0; i < N; i++) {
+          jogador1 = leitor.next();
+          jogador2 = leitor.next();
+          if (jogador1.equalsIgnoreCase("ataque") && jogador2.equalsIgnoreCase("ataque")) System.out.println("Aniquilacao mutua");
+          else if (jogador1.equalsIgnoreCase("papel") && jogador2.equalsIgnoreCase("papel")) System.out.println("Ambos venceram");
+          else if (jogador1.equalsIgnoreCase("ataque")) {
+              System.out.println("Jogador 1 venceu");
+          } else if (jogador2.equalsIgnoreCase("ataque")) {
+              System.out.println("Jogador 2 venceu");
+          } else if (jogador2.equalsIgnoreCase("papel")) {
+              System.out.println("Jogador 1 venceu");
+          } else if (jogador1.equalsIgnoreCase("papel")) {
+              System.out.println("Jogador 2 venceu");
+          } else {
+              System.out.println("Sem ganhador");
+      
+  }
+
+}
+}
+
+    @Override
+    public String toString() {
+        return "Testes []";
     }
 }
